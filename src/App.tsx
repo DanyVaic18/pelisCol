dfa3c90fdd5cea1095848c4efce41af7a37388f8
DanyVaic18 <loljose1090@gmail.com>
2023-03-03 03:11:45 -0500

    0.1.1 Pruebas con la API

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import "./App.css";
 import Home from "./components/Home";
 
 function App() {
-  const [first, setfirst] = useState<string>("");
-
   return (
     <>
       <Home></Home>
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
+  const API_LINK: string = "https://api.themoviedb.org/3";
+  const API_KEY: string = "cc39ef0542db89480ea0c89ffe75b641";
+  const IMG_LINK: string = "https://image.tmdb.org/t/p/original";
 
-    const API_LINK:string = ''
+  const [moviesArray, setMoviesArray] = useState<any>([]);
 
+  function handleFetchMovies(req: any): void {
+    fetch(`${API_LINK}/discover/movie/?api_key=${API_KEY}`)
+      .then((rep) => rep.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error("Error:", error));
+  }
 
+  useEffect(() => {
+    handleFetchMovies("a");
+    console.log(moviesArray);
+  }, []);
 
-  return (
-    <div className='bg-black text-white'>Home</div>
-  )
-}
+  return <div className="bg-black text-white">Home</div>;
+};
 
-export default Home
+export default Home;
